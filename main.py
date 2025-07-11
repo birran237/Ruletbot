f7c1e06b30a8100c2c2474d6913290e5035729d1
bielf <bielfloresmartinez@gmail.com>
2025-07-11 17:03:00 +0200

    Added disable bot support

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 import os
 import webserver
 import database
-from asyncio import create_task
+from asyncio import create_task, sleep
+from time import time
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -18,17 +19,6 @@ intents = discord.Intents.default()
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-async def timeout(interaction: discord.Interaction,user:discord.Member):
-    minutes = create_task(database.get_from_database(guild_id=interaction.guild_id, field="timeout_minutes", default=5))
-    affect_admins = create_task(database.get_from_database(guild_id=interaction.guild_id, field="annoy_admins", default=True))
-    higher_role: bool = user.top_role >= interaction.guild.me.top_role
-    if not user.resolved_permissions.administrator and not higher_role:
-        await user.timeout(timedelta(minutes= await minutes), reason="Ha perdido")
-        return
-    if higher_role and not await affect_admins:
-        return
-    await user.move_to(channel=None, reason="Ha perdido")
-
 
 async def tirar_rulet(interaction: discord.Interaction, user:discord.Member):
     if interaction.user.id == user.id or user.bot:
@@ -36,6 +26,10 @@ async def tirar_rulet(interaction: discord.Interaction, user:discord.Member):
         await user.timeout(timedelta(minutes=10), reason="Es minguito el pobre")
         return
 
+    disabled_time = get_disabled_status(interaction.guild_id)
+    if disabled_time is not None:
+        await interaction.response.send_message(f"He sido deshabilitado por los administradores hasta dentro de **{disabled_time//3600}horas, {disabled_time%3600//60} minutos y {disabled_time%60} segundos**.")
+        return
     if bool(random.randint(0, 1)):
         await interaction.response.send_message(f"{interaction.user.display_name} ha retado a un duelo a {user.mention} y ha ganado")
         await timeout(interaction, user)
@@ -43,6 +37,26 @@ async def tirar_rulet(interaction: discord.Interaction, user:discord.Member):
         await interaction.response.send_message(f"{interaction.user.display_name} ha retado a un duelo a {user.mention} y ha perdido")
         await timeout(interaction, interaction.user)
 
+async def timeout(interaction: discord.Interaction,user:discord.Member):
+    minutes = create_task(database.get_from_database(guild_id=interaction.guild_id, field="timeout_minutes", default=5))
+    affect_admins = create_task(database.get_from_database(guild_id=interaction.guild_id, field="annoy_admins", default=True))
+    higher_role: bool = user.top_role >= interaction.guild.me.top_role
+        if not user.resolved_permissions.administrator and not higher_role:
+        await user.timeout(timedelta(minutes= await minutes), reason="Ha perdido")
+        return
+    if higher_role and not await affect_admins:
+        return
+    await user.move_to(channel=None, reason="Ha perdido")
+
+def get_disabled_status(guild_id: int):
+    expire_at = disabled_servers.get(guild_id)
+    if not expire_at:
+        return None
+    remaining = expire_at - time()
+    if remaining <= 0:
+        disabled_servers.pop(guild_id, None)
+        return None
+    return remaining
 
 @bot.event
 async def on_ready():
@@ -65,6 +79,21 @@ async def rulet(interaction: discord.Interaction, persona: discord.Member):
 async def rulet_context(interaction: discord.Interaction, persona: discord.Member):
     await tirar_rulet(interaction, persona)
 
+
+disabled_servers = {}
+@app_commands.command(name="disable",description="Deshabilita el bot durante los minutos especificados")
+@app_commands.checks.has_permissions(administrator=True)
+@app_commands.describe(minutes="Cantidad de minutos (deja en 0 para habilitar el bot instantaniamente)")
+async def disable(interaction: discord.Interaction, minutes: app_commands.Range[float, 0, 1440]):
+    expire_at = time() + (minutes * 60)
+    disabled_servers[interaction.guild_id] = expire_at
+
+    async def enable():
+        await sleep(minutes*60)
+        if disabled_servers.get(interaction.guild_id) == expire_at:
+            del disabled_servers[interaction.guild_id]
+
+    create_task(enable())
 class SetGroup(app_commands.Group):
     @app_commands.command(name="timeout", description="Configura los minutos de timeout de la rulet (deja en blanco para saber la configuración actual)")
     @app_commands.checks.has_permissions(administrator=True)
